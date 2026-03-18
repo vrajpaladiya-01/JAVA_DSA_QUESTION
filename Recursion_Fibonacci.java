0ecf491cd1808815ef3ece52fb831ec1b4e21803
vrajpaladiya <vrajpaladiya01@gmail>
2026-03-19 00:31:11 +0530

    first commit

--- Recursion_Fibonacci.java
+++ Recursion_Fibonacci.java
@@ -1,24 +1,49 @@
 public class Recursion_Fibonacci {
-    public static int fib(int n){
-        if(n == 0 || n ==1){
+
+    // Recursive Fibonacci
+
+
+    // public static int fib(int n){
+    //     if(n == 0 || n == 1){
+    //         return n;
+    //     }
+
+    //     return fib(n-1) + fib(n-2);
+    // }
+
+
+
+
+    // DP Fibonacci
+
+    public static int fib(int n, int f[]){
+        if(n == 0 || n == 1){
             return n;
         }
-        int Fnm1 = fib(n-1);
-        int Fnm2 = fib(n-2);
-        int fn = Fnm1 + Fnm2;
 
-        return fn;
+        if(f[n] != 0){
+            return f[n];
+        }
+        f[n] = fib(n-1, f) + fib(n-2, f);
+        return f[n];
     }
-    public static void main(String[] args) {
-        int n = 7;
 
-        System.out.println(fib(n));
-        System.out.println(fib(8));
-        System.out.println(fib(9));
-        System.out.println(fib(10));
-        System.out.println(fib(11));
-        System.out.println(fib(12));
-        System.out.println(fib(13));
-        
+    public static int fibTabulation(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+    public static void main(String args[]) {
+        int n = 7;
+        int f[] = new int[n+1];
+
+        System.out.println(fibTabulation(n));
+        // System.out.println(fib(n, f));        
     }
 }
