d48a9f14197deab13c89b588732c6f4dd756ad78
Vraj Paladiya <vrajpaladiya26@gmail.com>
2026-06-25 16:46:13 +0530

    LeetCode Q66 PlusOne

---
+++ PluseOne_L66.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PluseOne_L66 {
+    public static int[] pluseOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+
+        int[] result = pluseOne(digits);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
